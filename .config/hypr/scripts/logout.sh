941caffce72501f2626f97fe509afd316fc9f659
Deyusha69 <98688116+Deyusha69@users.noreply.github.com>
2023-02-14 23:09:35 +0800

    Add files via upload

--- .config/hypr/scripts/logout.sh
+++ .config/hypr/scripts/logout.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-wlogout --protocol layer-shell -b 5 -T 400 -B 400
+#!/usr/bin/env bash
+
+LAYOUT="$HOME/.config/wlogout/layout"
+STYLE="$HOME/.config/wlogout/style.css"
+
+if [[ ! `pidof wlogout` ]]; then
+    wlogout --layout ${LAYOUT} --css ${STYLE} \
+        --buttons-per-row 3 \
+        --column-spacing 50 \
+        --row-spacing 50 \
+        --margin-top 290 \
+        --margin-bottom 290 \
+        --margin-left 300 \
+        --margin-right 300
+else
+    pkill wlogout
+fi
