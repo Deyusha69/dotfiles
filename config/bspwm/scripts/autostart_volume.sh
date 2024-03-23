72d9178981a123fde6b9d8821254f88b18f41665
Deyusha69 <98688116+Deyusha69@users.noreply.github.com>
2024-03-23 08:58:21 +0000

    Add files via upload

---
+++ config/bspwm/scripts/autostart_volume.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sleep 10 && pactl set-sink-volume @DEFAULT_SINK@ 118%
---
+++ config/bspwm/scripts/record1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Check if FFmpeg is already running
+if pgrep ffmpeg > /dev/null; then
+    # If FFmpeg is running, kill the process
+    pkill ffmpeg
+    notify-send "Recording stopped"
+else
+    # If FFmpeg is not running, start recording
+    ffmpeg -f x11grab -video_size 1600x900 -i :0.0+0,0 -f alsa -i default -preset ultrafast "/home/meth/Videos/rec_$(date +%b-%d-%I:%M:%S).mp4" &
+    notify-send "Recording started"
+fi
